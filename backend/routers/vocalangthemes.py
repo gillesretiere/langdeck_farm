578fda48b0979871675c8c48be5df9804171137e
gillesretiere <120189247+gillesretiere@users.noreply.github.com>
2023-03-15 18:01:30 +0100

    Vocabularies
    .

--- backend/routers/vocalangthemes.py
+++ backend/routers/vocalangthemes.py
@@ -12,14 +12,14 @@ async def list_available_themes(request:Request, theme_uid: Optional[str]=None)-
     query={}
     if theme_uid:
         query["theme_uid"] = theme_uid
-    full_query = request.app.mongodb["vocabularies_language_theme"].find(query).sort("theme_uid",1)
+    full_query = request.app.mongodb["vocabularies"].find(query).sort("theme_uid",1)
     result = [VocabulariesLanguageThemeDB(**raw_theme) async for raw_theme in full_query]
     return result
 
 #--- list one theme by uid
 @router.get("/{uid}", response_description="List all themes by language uid")
 async def list_themes_by_language_id (request: Request, uid: str):
-    if (language_uid := await request.app.mongodb["vocabularies_language_theme"].find_one({"language_uid":uid})) is not None:
+    if (language_uid := await request.app.mongodb["vocabularies"].find_one({"language_uid":uid})) is not None:
         return VocabulariesLanguageThemeDB(**language_uid)
     raise HTTPException (status_code=404, detail=f"Language with uid {uid} not found.") 
 
